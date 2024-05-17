bcea819052b55ccdf51e0f96bba10ab7601d7555
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2024-05-17 11:54:22 +0530

    test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/v1", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/v1/test", (req, res) => {
+    res.send("Hello test!");
 });
 
 app.listen(process.env.PORT, () => {
